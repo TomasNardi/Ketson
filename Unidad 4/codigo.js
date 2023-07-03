b4f170223864365d95cd2991651bed4419bd00de
Tomas Nardi <tomas.nardi@hotmail.com>
2023-07-03 20:16:57 -0300

    unidad 4

---
+++ Unidad 4/codigo.js
@@ -0,0 +1,27 @@
+/* ejercicio 1 
+let recorrer = prompt("cuantos metros tenes que recorrer?");
+    if (recorrer >= 0 && recorrer <1000) { 
+    alert ("Recorrelo a pie")} 
+
+    else if (recorrer >= 1000 && recorrer <10000) { 
+    alert ("Recorrelo en bici")} 
+
+    else if (recorrer >= 10000 && recorrer <30000) { 
+    alert ("Recorrelo en colectivo")}
+
+    else if (recorrer >= 30000 && recorrer <100000) { 
+    alert ("Recorrelo en auto")}
+
+    else if (recorrer > 100000) { 
+    alert ("Recorrelo en avion")}
+
+    
+    alert ("Escribi un valor numerico")
+*/
+
+
+/* ejercicio 2
+
+Array = [1,2,324,5,52,423,2,45,24]
+let numeroMaximo = Math.max(...Array)
+document.write (numeroMaximo)
